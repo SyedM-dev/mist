c69bf59a71541c0aff03804dcee332a893bec4cf
Daanish <me@syedm.dev>
2026-03-29 22:24:14 +0000

    Keep minimum wood/metal to allow razor to always be craftable

--- game/props/chest.rb
+++ game/props/chest.rb
@@ -4,6 +4,6 @@ class Chest < Prop
   setup_sprites("assets/images/chest.png", 1)
 
   def resources
-    { wood: rand(0..64), metal: rand(0..16), science: rand(0..4) }
+    { wood: rand(2..64), metal: rand(1..16), science: rand(0..4) }
   end
 end
